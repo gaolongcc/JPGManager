14e6c7d9743ce3f6bb001814d24c195f5e37ec93
gaolongcc <gaolongcc@outlook.com>
2018-05-04 11:33:33 +0800

    third version

--- JPG_GDIManager/ComFunction.cpp
+++ JPG_GDIManager/ComFunction.cpp
@@ -40,24 +40,27 @@ std::string ComFunction::ConvertWStringToAnsi(const std::wstring& wstr)
 std::wstring ComFunction::ConvertAnsiToWString(const std::string& str)
 {
     std::wstring result;
-
     int len = MultiByteToWideChar(CP_ACP, 0, str.c_str(), str.size(), NULL, 0);
     if (len < 0)
+    {
         return result;
-
+    }
     wchar_t* buffer = new wchar_t[len + 1];
     if (buffer == NULL)
+    {
         return result;
-
+    } 
     MultiByteToWideChar(CP_ACP, 0, str.c_str(), str.size(), buffer, len);
-
-    buffer[len] = '\0';                    //×Ö·û´®¶ÏÎ²
-    result.append(buffer);                 //¸³Öµ
-    delete[] buffer;                       //É¾³ý»º³åÇø
-
+    //×Ö·û´®¶ÏÎ²
+    buffer[len] = '\0';  
+    //¸³Öµ
+    result.append(buffer);  
+    //É¾³ý»º³åÇø
+    delete[] buffer;                       
     //·µ»ØÖµ
     return result;
 }
+
 std::wstring ComFunction::GetFilePath(std::wstring& strFullPathName)
 {
     if (strFullPathName.empty())
@@ -65,72 +68,77 @@ std::wstring ComFunction::GetFilePath(std::wstring& strFullPathName)
         return _T("");
     }
     std::string::size_type iPos = strFullPathName.find_last_of(_T('\\'));
-
     return strFullPathName.substr(0, iPos);
 }
 
-// ÅÐ¶ÏÊäÈëµÄÊÇ·ñÎªÊý×Ö
-bool ComFunction::IsNumber(const std::string &str)
+// ÅÐ¶ÏÊäÈëµÄÊÇ·ñÎª¸¡µãÊý×Ö
+bool ComFunction::IsFloatNumber(const std::string &str)
 {
-    if (str.size()>2)
+    bool bflagPoint = true;
+    if (str.size() > 8)
     {
-        if (str.c_str()[0] == '0' && str.c_str()[1] == '.')
-        {
-            return true;
-
-        }
-        else
-        {
-            if (str.c_str()[0] != '.')
-            {
-                for (size_t i = 0; i < str.size(); i++)
-                {
-                    if (str.c_str()[i] < '0' || str.c_str()[i] > '9')
-                    {
-                        return false;
-                    }
-                }
-                return true;
-            }
-            else
-            {
-                for (size_t i = 1; i < str.size(); i++)
-                {
-                    if (str.c_str()[i] < '0' || str.c_str()[i] > '9')
-                    {
-                        return false;
-                    }
-                }
-                return true;
-            }
-
-        }
+        return false;
     }
-    else
+    if (str.size() > 0)
     {
-        if (str.c_str()[0] != '.')
+        if (str.c_str()[0] != '0')
         {
             for (size_t i = 0; i < str.size(); i++)
             {
+                if (str.c_str()[i] == '.' && bflagPoint)
+                {
+                    bflagPoint = false;
+                    continue;
+                }
                 if (str.c_str()[i] < '0' || str.c_str()[i] > '9')
                 {
                     return false;
                 }
+                else
+                {
+                    continue;
+                }
             }
-            return true;
         }
         else
         {
             for (size_t i = 1; i < str.size(); i++)
             {
+                if (str.c_str()[i] == '.' && bflagPoint)
+                {
+                    bflagPoint = false;
+                    continue;
+                }
                 if (str.c_str()[i] < '0' || str.c_str()[i] > '9')
                 {
                     return false;
                 }
+                else
+                {
+                    continue;
+                }
             }
-            return true;
-        }
-
+        }     
     }
+    return true;
 }
 
+// ÅÐ¶ÏÊäÈëµÄÊÇ·ñÎªÕûÐÍÊý×Ö
+bool ComFunction::IsIntNumber(const std::string &str)
+{
+    if (str.size() > 8)
+    {
+        return false;
+    }
+    if (str.size() > 0)
+    {
+        for (size_t i = 0; i < str.size(); i++)
+        {
+            if (str.c_str()[i] < '0' || str.c_str()[i] > '9')
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
--- JPG_GDIManager/ComFunction.h
+++ JPG_GDIManager/ComFunction.h
@@ -6,9 +6,29 @@ public:
     ComFunction();
     ~ComFunction();
 
+    /** 宽字符转字符
+    @param[in] wstr 宽字符字符串
+    */
     static std::string ConvertWStringToAnsi(const std::wstring& wstr);
+
+    /** 字符转宽字符
+    @param[in] str 字符字符串
+    */
     static std::wstring ConvertAnsiToWString(const std::string& str);
-    static bool IsNumber(const std::string &str);
+
+    /** 检查浮点数字
+    @param[in] str 字符串
+    */
+    static bool IsFloatNumber(const std::string &str);
+
+    /** 检查整型数字
+    @param[in] str 字符串
+    */
+    static bool IsIntNumber(const std::string &str);
+
+    /** 获得文件夹路径
+    @param[in] strFullPathName 文件夹中文件路径
+    */
     static std::wstring GetFilePath(std::wstring& strFullPathName);
 };
 
